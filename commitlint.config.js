9d398fe2c97bd08d4f7afee38fe08a9ddb754826
Muhammad Armghan Baig <armg.baig@gmail.com>
2026-08-17 18:32:05 +0500

    chore(config1): configure husky, lint-stages, and commitlint for this project

---
+++ commitlint.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "scope-empty": [2, "never"],
+    "scope-enum": [
+      2,
+      "always",
+      ["api", "owner", "auth", "driver", "config", "deps", "assets"],
+    ],
+  },
+};
