22f40e6f979e7412e4ab43fbdf15a800be49b1ab
Muhammad Armghan Baig <armg.baig@gmail.com>
2026-09-03 19:02:56 +0500

    feat(restaurants): refactor business settings screen with skeletons and reusable components

--- src/features/settings/components/index.ts
+++ src/features/settings/components/index.ts
@@ -1,2 +1,10 @@
-export { default as BusinessInfoCard } from "./BusinessInfoCard";
-export { default as BusinessScheduleCard } from "./BusinessScheduleCard";
+export {
+  default as BusinessInfoCard,
+  type IBusinessInfoCardProps,
+} from "./BusinessInfoCard";
+export {
+  default as BusinessScheduleCard,
+  type IBusinessScheduleCardProps,
+} from "./BusinessScheduleCard";
+export { default as BusinessInfoCardSkeleton } from "./skeletons/BusinessInfoCardSkeleton";
+export { default as BusinessScheduleCardSkeleton } from "./skeletons/BusinessScheduleCardSkeleton";
