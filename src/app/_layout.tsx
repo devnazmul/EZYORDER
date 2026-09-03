cc6fbbebad0583d8ec574cbf7152622785cc646f
Muhammad Armghan Baig <armg.baig@gmail.com>
2026-09-03 20:15:06 +0500

    feat(shared): integrate roboto font family

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,15 @@
-import LoadingScreen from "@/components/reuseable/LoadingScreen";
-import { useAuth, usePushNotifications } from "@/hooks";
-import { AuthProvider, ResponsiveProvider } from "@/src/context";
-
+import {
+  Roboto_300Light,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+  Roboto_900Black,
+  useFonts,
+} from "@expo-google-fonts/roboto";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import * as NavigationBar from "expo-navigation-bar";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
 import { Platform } from "react-native";
@@ -12,7 +17,14 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 
+import LoadingScreen from "@/components/reuseable/LoadingScreen";
+import { useAuth, usePushNotifications } from "@/hooks";
+import { AuthProvider, ResponsiveProvider } from "@/src/context";
+
 import "../styles/global.css";
+
+// Prevent splash screen from hiding automatically until assets are loaded
+SplashScreen.preventAutoHideAsync();
 
 const queryClient = new QueryClient();
 
@@ -37,12 +49,30 @@ function RootNavigation() {
 }
 
 export default function RootLayout() {
+  const [fontsLoaded, fontError] = useFonts({
+    Roboto_300Light,
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+    Roboto_900Black,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded || fontError) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
   useEffect(() => {
     if (Platform.OS === "android") {
       NavigationBar.setBackgroundColorAsync("#FFFFFF");
       NavigationBar.setButtonStyleAsync("dark");
     }
   }, []);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
